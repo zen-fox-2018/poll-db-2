de6a26e4c4ad5d8c3b7c51021b257b6a01d9b653
taqiabdulaziz <svx.annexiv@gmail.com>
2018-12-10 16:13:17 +0700

    komit

---
+++ db.js
@@ -0,0 +1,4 @@
+const sqlite3 = require(`sqlite3`).verbose()
+let db = new sqlite3.Database(`database.db`)
+
+module.exports = db
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const db = require(`../poll-db-2/db`)
+const Table = require(`cli-table`)
 //NO 1
 let query1 = `SELECT P.nama, P.location, P.grade_current, COUNT(*) AS TotalVote FROM Politicians P JOIN Votes Vt ON P.id = Vt.politician_id 
 GROUP BY P.nama HAVING P.grade_current < 9 ORDER BY P.grade_current ASC`
@@ -25,14 +27,89 @@ let query2 = `SELECT
 ON table_voter.politician_id = table_politician.id 
 ORDER BY table_politician.nama DESC, TotalVote DESC`
 
-let query3 = `SELECT COUNT(*) AS totalVote, Vr.first_name || " " || Vr.last_name AS name, Vr.gender, vr.age 
-FROM Voters Vr 
-JOIN Votes Vt ON Vr.id = Vt.voter_id
-GROUP BY Vr.first_name HAVING totalVote > 1
-ORDER BY totalVote DESC`
 
-let query4 = `SELECT  COUNT(Votes.politician_id) AS TotalVotes,Voters.first_name||' '||Voters.last_name AS name, gender, age
+let query3 = `SELECT  COUNT(Votes.politician_id) AS TotalVotes,Voters.first_name||' '||Voters.last_name AS name, gender, age
 FROM Voters JOIN Votes ON Voters.Id = Votes.voter_id
 GROUP BY Voters.id 
 HAVING TotalVotes > 1
 ORDER BY TotalVotes DESC`
+
+
+db.all(query1, function (err, rows) {
+    var table = new Table({
+        chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
+               , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
+               , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
+               , 'right': '║' , 'right-mid': '╢' , 'middle': '│' }
+      });
+    let header = []
+    let row = []
+
+    for (const key in rows[0]) {
+        header.push(key)
+    }
+    table.push(header)
+
+    for (let i = 0; i < rows.length; i++) {
+        for (const key in rows[i]) {
+            row.push(rows[i][key])
+        }
+        table.push(row)
+        row = []
+    }
+    console.log(`                           NO 1                     `);  
+    console.log(table.toString());
+      
+})
+db.all(query2, function (err, rows) {
+    var table = new Table({
+        chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
+               , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
+               , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
+               , 'right': '║' , 'right-mid': '╢' , 'middle': '│' }
+      });
+    let header = []
+    let row = []
+
+    for (const key in rows[0]) {
+        header.push(key)
+    }
+    table.push(header)
+
+    for (let i = 0; i < rows.length; i++) {
+        for (const key in rows[i]) {
+            row.push(rows[i][key])
+        }
+        table.push(row)
+        row = []
+    }
+    console.log(`                           NO 2                     `);  
+    console.log(table.toString());
+      
+})
+db.all(query3, function (err, rows) {
+    var table = new Table({
+        chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
+               , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
+               , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
+               , 'right': '║' , 'right-mid': '╢' , 'middle': '│' }
+      });
+    let header = []
+    let row = []
+
+    for (const key in rows[0]) {
+        header.push(key)
+    }
+    table.push(header)
+
+    for (let i = 0; i < rows.length; i++) {
+        for (const key in rows[i]) {
+            row.push(rows[i][key])
+        }
+        table.push(row)
+        row = []
+    }
+    console.log(`                           NO 3                     `);  
+    console.log(table.toString());
+      
+})
